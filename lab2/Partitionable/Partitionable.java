a2cb7aefc3540d7dc09eeb6670742851b25c0b1b
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-04 20:34:47 +0800

    Finish tasks

--- lab2/Partitionable/Partitionable.java
+++ lab2/Partitionable/Partitionable.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class Partitionable {
@@ -10,9 +11,49 @@ public class Partitionable {
      * @return true iff list is partitionable.
      */
     public static boolean isPartitionable(List<Integer> list) {
-        
-		
-		
-        return true;
+        int leftHandSum = 0;
+        int rightHandSum = 0;
+        int leftHandPointer = 0;
+        int rightHandPointer = list.size()-1;
+
+        if(list.size() == 1)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < list.size()+1; i++)
+        {
+
+
+
+            if(leftHandSum == rightHandSum)
+            {
+                if(rightHandPointer + 1 == leftHandPointer){ return true;}
+                else {
+                    leftHandSum += list.get(leftHandPointer);
+                    leftHandPointer += 1;
+                }
+            }else if(leftHandSum - rightHandSum > 0)
+            {
+                rightHandSum += list.get(rightHandPointer);
+                rightHandPointer -= 1;
+            }
+            else if(rightHandSum - leftHandSum > 0)
+            {
+                leftHandSum += list.get(leftHandPointer);
+                leftHandPointer +=1;
+            }
+        }
+        return false;
     }
+
+    public static void main(String[] args)
+    {
+        List<Integer> list = Arrays.asList(123);
+        System.out.println(isPartitionable(list));
+    }
+
+
+
+
 }
