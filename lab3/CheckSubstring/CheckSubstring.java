2000a793b9df9433ae1c5782323386949d45a28c
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-14 22:27:42 +0800

    CheckSubstring finished.

--- lab3/CheckSubstring/CheckSubstring.java
+++ lab3/CheckSubstring/CheckSubstring.java
@@ -1,22 +1,47 @@
 public class CheckSubstring {
-	
+
     /**
      * Check if at least n copies of a substring occur in input string.
      * They may overlap.
      * For example, checkSubstring("ababaxxxaba", "aba", 3) → true
+     *
      * @param input is the input string.
-     * @param subs is the non-empty substring.
-     * @param n is non-negative number of occurrences.
+     * @param subs  is the non-empty substring.
+     * @param n     is non-negative number of occurrences.
      * @return true iff at least n copies of subs occur in input.
      */
     public static boolean checkSubstring(String input, String subs, int n) {
         // base case
-		
-		
-		
-        // recursive step
-		
-		
-		return false;
+        if(input.length() < subs.length())
+        {
+            if(n<=0)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+
+        if(input.substring(0,subs.length()).equals(subs))
+                return checkSubstring(input.substring(1),subs, n-1);
+        return checkSubstring(input.substring(1),subs,n);
     }
+
+
+
+    public static void main(String[] args)
+    {
+        String input = "abcxxxxabc";
+        String subs = "abc";
+        int n = 2;
+        System.out.println(checkSubstring(input, subs, n));
+
+    }
+
+
 }
+
+
+
+
