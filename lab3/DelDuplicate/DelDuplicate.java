b803e5205beaaebf1d79f0e548829f627a13cbee
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-15 09:37:40 +0800

    DelDuplicate finished

--- lab3/DelDuplicate/DelDuplicate.java
+++ lab3/DelDuplicate/DelDuplicate.java
@@ -7,13 +7,34 @@ public class DelDuplicate {
      * @return the resulting string.
      */
     public static String delDuplicate(String input) {
-        // base case
-        
-		
-		
-        // recursive step
-        
-		
-		return null;
+        if(input.length()==0)
+        {
+            return input;
+        }else {
+            return delDuplicateHelper(input, 0, "");
+        }
+    }
+
+    public static String delDuplicateHelper(String input,int pos, String buildString)
+    {
+        if(pos > input.length()-2)
+        {
+            buildString += Character.toString(input.charAt(pos));
+            return buildString;
+        }
+
+        if(input.charAt(pos)!=input.charAt(pos+1))
+        {
+            buildString += Character.toString(input.charAt(pos));
+
+        }
+
+        return delDuplicateHelper(input,pos+1,buildString);
+    }
+
+    public static void main(String[] args)
+    {
+        String input = "abaaaaasddddddsdsssdssdwweqweqqq";
+        System.out.println(delDuplicate(input));
     }
 }
