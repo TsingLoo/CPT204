f4b5a2b5acae95d713f9b000e64ca70c6f580fb1
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-16 11:29:50 +0800

    Finished

--- lab3/OddAndTen/OddAndTen.java
+++ lab3/OddAndTen/OddAndTen.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class OddAndTen {
@@ -12,17 +13,36 @@ public class OddAndTen {
      */
     public static boolean oddAndTen(List<Integer> list) {
         // call your recursive helper method
-        return false;
+        return oddAndTenHelper(0, list,0,0);
     }
 
-    private static boolean oddAndTenHelper() { // add any parameters
-        // base case
-        
-		
-		
-        // recursive step
-        
-		
-		return false;
+    private static boolean oddAndTenHelper(int start, List<Integer> list, int LeftSum, int RightSum)
+    {
+        if(list.size() == 1)
+        {
+
+        }
+
+        if (start >= list.size()) {
+            return LeftSum % 10 == 0 && RightSum % 2 == 1;
+        }
+
+        // 2. Recursive case: assign a number to either first group or second group.
+        int thisNum = list.get(start);
+        // 2.1 Try assigning the number to first group
+        if ( oddAndTenHelper(start+1, list, LeftSum+thisNum, RightSum) )
+            return true; // success!
+
+        // 2.2 Try assigning it to second group
+        // this is the last case, so return whatever the recursive call returns
+        return oddAndTenHelper(start+1, list, LeftSum, RightSum+thisNum);
     }
+
+    public static void main(String[] args)
+    {
+        List<Integer> list = Arrays.asList(0);
+        System.out.println(oddAndTen(list));
+    }
+
+
 }
