8c9c92e61f8720f9e2a7821f88bc47998b58439c
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-30 08:42:15 +0800

    Week 6 init

---
+++ lab5/LLDeque/LLDeque.java
@@ -0,0 +1,157 @@
+public class LLDeque<T> {
+
+    private class Node {
+        Node prev;
+        T item;
+        Node next;
+
+        Node(Node p, T i, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
+    /**
+     * @return the number of items in the deque.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * @return true if deque is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /*
+     ***************************
+     * DO NOT MODIFY CODE ABOVE
+     ***************************
+     */
+
+
+    // LAB EXERCISE 5.1 EMPTY CONSTRUCTOR
+
+    /**
+     * Creates an empty deque.
+     */
+    public LLDeque() {
+		
+		
+		
+    }
+
+
+    // LAB EXERCISE 5.2 ADD TO FRONT
+
+    /**
+     * Adds an item of type T to the front of the deque.
+     * @param item is a type T object added to the deque.
+     */
+    public void addFirst(T item) {
+		
+		
+		
+    }
+
+
+    // LAB EXERCISE 5.3 PRINT ITEMS
+
+    /**
+     * Prints the items in the deque from first to last,
+     * separated by a space, ended with a new line.
+     */
+    public void printDeque() {
+		
+		
+		
+    }
+
+
+    // LAB EXERCISE 5.4 ITERATIVE GET ITEM
+
+    /**
+     * Gets the item at the given index.
+     * If no such item exists, returns null.
+     * Does not mutate the deque.
+     * @param index is an index where 0 is the front.
+     * @return the ith item of the deque, null if it does not exist.
+     */
+    public T iterGet(int index) {
+		
+		
+		return null;
+    }
+
+
+    // EXERCISE 5.1 ADD TO BACK
+
+    /**
+     * Adds an item of type T to the back of the deque.
+     * @param item is a type T object added to the deque.
+     */
+    public void addLast(T item) {
+		
+		
+		
+    }
+
+
+    // EXERCISE 5.2 DELETE FRONT
+
+    /**
+     * Deletes and returns the item at the front of the deque.
+     * If no such item exists, returns null.
+     * @return the first item of the deque, null if it does not exist.
+     */
+    public T delFirst() {
+		
+		
+		return null;
+    }
+
+
+    // EXERCISE 5.3 DELETE BACK
+
+    /**
+     * Deletes and returns the item at the back  of the deque.
+     * If no such item exists, returns null.
+     * @return the last item of the deque, null if it does not exist.
+     */
+    public T delLast() {
+		
+		
+		return null;
+    }
+
+
+    // EXERCISE 5.4 RECURSIVE GET ITEM
+
+    /**
+     * Gets the item at the given index.
+     * If no such item exists, returns null.
+     * Does not mutate the deque.
+     * @param index is an index where 0 is the front.
+     * @return the ith item of the deque, null if it does not exist.
+     */
+    public T recGet(int index) {
+		
+		
+		return null;
+    }   
+	
+
+    public static void main(String[] args) {
+        LLDeque<String> deque = new LLDeque<>();
+        deque.addFirst("b");
+        deque.addFirst("a");
+        deque.printDeque();
+    }
+
+}
