9254477e82121dadee1764c4856673dd2bf6e364
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-04-10 11:16:25 +0800

    printArrayFromTailtoHead Finished by ArrayList

--- Code/printArrayFromTailtoHead.java
+++ Code/printArrayFromTailtoHead.java
@@ -12,31 +12,24 @@ class ListNode {
 
 public class printArrayFromTailtoHead {
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        ArrayList<Integer> answerList = new ArrayList<Integer>();
-        Stack<Integer> stack = new Stack<Integer>();
+        ArrayList<Integer> answerList = new ArrayList<>();
         while(listNode != null)
         {
-            stack.push(listNode.val);
-            listNode = listNode.next;
+            answerList.add(0,listNode.val);
+            listNode= listNode.next;
         }
-        while(!stack.empty())
-        {
-            answerList.add(stack.pop());
-        }
+
         return answerList;
     }
 
     public static void main(String[] args)
     {
 
-        ArrayList<Integer> testList = new ArrayList<Integer>();
+        ArrayList<Integer> testList = new ArrayList<>();
         testList.add(1);
         testList.add(2);
         testList.add(3);
         testList.add(4);
-
-
-
 
     }
 }
