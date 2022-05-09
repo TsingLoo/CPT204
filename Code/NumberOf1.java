cd6d754a90f48f2e1f5fb016b22baffcb9ac88e0
TsingLoo <xiaonan.pan19@student.xjtlu.edu.cn>
2022-05-09 08:10:43 +0800

    NumberOf1 finished https://www.nowcoder.com/practice/8ee967e43c2c4ec193b040ea7fbb10b8

--- Code/NumberOf1.java
+++ Code/NumberOf1.java
@@ -8,13 +8,11 @@ public class NumberOf1 {
     public static int NumberOf1Method(int n) {
 
         int count = 0;
-        for(int i=0;i<32;i++)
-        {
-
-            count = count + (n&1);
-            n = n >>1;
-
+        while(n != 0) {
+            n = n&(n-1);
+            ++count;
         }
+
 
         return count;
     }
