bba4e3da68907d63b4649d18f68cee295f8c8673
TsingLoo <xiaonan.pan19@student.xjtlu.edu.cn>
2022-05-10 20:49:02 +0800

    Lab Exercise 11.2 Dog EQUALS Finished

---
+++ lecture11_and_lab11/Dog/Dog.java
@@ -0,0 +1,55 @@
+public class Dog implements Comparable<Dog> {
+
+    private String name;
+    private int weight;
+
+    public Dog(String n, int w) {
+        name = n;
+        weight = w;
+    }
+
+    /**
+     * @param other another Dog
+     * @return negative number if this is smaller than other
+     *         0 if this equals other
+     *         positive number if this is larger than other
+     */
+    @Override
+    public int compareTo(Dog other) {
+
+//        if (this.weight < other.weight) {
+//            return -1;
+//        } else if (this.weight == other.weight) {
+//            return 0;
+//        }
+//        return 1;
+
+        return this.weight - other.weight;
+
+    }
+
+    public void bark() {
+        System.out.println(name + ": bark!");
+    }
+
+
+    /*
+     ***************************
+     * DO NOT MODIFY CODE ABOVE
+     ***************************
+     */
+    // LAB EXERCISE 11.2  EQUALS
+
+    @Override
+    public boolean equals(Object other) {
+        if(other == null) return  false;
+        if(!(other instanceof Dog)) return false;
+        Dog that = (Dog)other;
+		if((this.name).equals(that.name) && (this.weight)==(that.weight))
+        {
+            return  true;
+        }
+        return  false;
+    }
+
+}
