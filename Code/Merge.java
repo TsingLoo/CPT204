e69693eb8baff8fe13b57c08d26cd69dd231eb3b
TsingLoo <xiaonan.pan19@student.xjtlu.edu.cn>
2022-05-12 09:30:03 +0800

    Merge Finished by sentinel https://www.nowcoder.com/practice/d8b6b4358f774294a89de2a6ac4d9337

--- Code/Merge.java
+++ Code/Merge.java
@@ -11,21 +11,38 @@ public class Merge {
     }
     public ListNode Merge(ListNode list1,ListNode list2) {
 
-      if(list1 == null || list2 == null)
-      {
-          return  list1 != null? list1: list2;
-      }
+        if(list1 == null || list2 == null)
+        {
+            return  list1 != null? list1: list2;
+        }
 
-      if(list1.val<= list2.val)
-      {
-          list1.next = Merge(list1.next ,list2);
-          return  list1;
-      }else
-      {
-          list2.next = Merge(list2.next,list2);
-          return  list2;
-      }
+        ListNode result = new ListNode(0);
+        ListNode head = result;
+        while(list1!=null&&list2!=null)
+        {
+            if(list1.val<= list2.val)
+            {
+                result.next = list1;
+                list1 = list1.next;
+                result = result.next;
+            }else
+            {
+                result.next = list2;
+                list2 = list2.next;
+                result = result.next;
+            }
+        }
 
+        if(list1!=null)
+        {
+            result.next = list1;
+        }else
+        {
+            result.next = list2;
+        }
+
+        //result.next = list2;
+        return  head.next;
 
     }
 }
